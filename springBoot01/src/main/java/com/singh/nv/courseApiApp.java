52b30643ea74aaba22e228590b725076b48a8837
Nisha Singh <nishavsingh@Nishas-MBP.attlocal.net>
2022-12-04 21:12:02 -0800

    added springbootapplication

---
+++ springBoot01/src/main/java/com/singh/nv/courseApiApp.java
@@ -0,0 +1,11 @@
+package com.singh.nv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class courseApiApp {
+     public static void main(String[] args) {
+         SpringApplication.run(courseApiApp.class,args);
+    }
+}
